3ea65c6c78bfb7d84601de1d2fa5f4f78aa3151a
Eric Shelbogashev <e.shelbogashev@gmail.com>
2023-02-10 13:35:57 +0700

    Fixed random

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "boost/mpi.hpp"
+#include "random"
 
 namespace mpi = boost::mpi;
 
@@ -11,24 +12,22 @@ int main(int argc, char *argv[]) {
     mpi::communicator world;
     std::cout << "Process with rank " << world.rank() << " was initialized." << std::endl;
     if (world.rank() == 0) {
-        // Pick a random amount of integers to send to process one
-        number_amount = (rand() / (float)RAND_MAX) * MAX_NUMBERS;
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> distMN(1,MAX_NUMBERS); // distribution in range [1, 6]
+        number_amount = static_cast<int>(distMN(rng));
         for (int i = 0; i < number_amount; ++i) {
             numbers[i] = number_amount - i;
             std::cout << number_amount - i << " ";
         }
         std::cout << std::endl;
 
-        // Send the amount of integers to process one
         world.send(1, 0, numbers, number_amount);
         std::cout << "0 sent " << number_amount << " numbers to 1" << std::endl;
     } else if (world.rank() == 1) {
-        // Receive at most MAX_NUMBERS from process zero
+
         mpi::status status = world.recv(0, 0, numbers, 200);
-        // After receiving the message, check the status to determine
-        // how many numbers were actually received
-        // Print off the amount of numbers, and also print additional
-        // information in the status object
+
         if (status.count<int>().has_value()) {
             std::cout << "1 received " << status.count<int>().get() << " numbers from source =" << status.source() <<
                       ", tag =" << status.tag() << "." << std::endl;
